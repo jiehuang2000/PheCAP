accf73156ed87e93027680713398b1086582113e
Ming Yang <hustyangming@gmail.com>
2018-09-06 13:29:43 +0800

    update

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' Example EHR data with 10,000 records.
+#'
+#' A dataset containing the example to illustrate the main steps of the 
+#' algorithms and usage of functions in the package.
+#'
+#' @format A data frame with 10,000 rows and 588 variables:
+"ehrdata"
